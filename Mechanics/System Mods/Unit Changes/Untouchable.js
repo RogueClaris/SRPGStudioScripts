7168102baa6763ab3899e016a48e78657b3ad458
Claris <seventh_sage@hotmail.com>
2021-06-06 20:56:47 -0400

    Updating Untouchable.js

--- Mechanics/System Mods/Unit Changes/Untouchable.js
+++ Mechanics/System Mods/Unit Changes/Untouchable.js
@@ -1,10 +1,31 @@
-var RSU0 = PosSelector.getSelectorTarget;
-PosSelector.getSelectorTarget = function(isIndexArray) {
-	var unit = RSU0.call(this, isIndexArray);
-	
-	// Check if the unit exists at the cursor position and the unit exists within a range.
-	if (unit !== null && isIndexArray && unit.custom.Untouchable && unit.getUnitType() === UnitType.ENEMY){
-		unit = null;
+/*
+Add the custom parameter to the unit {Untouchable:true}
+They will not be targetable for actions at all.
+*/
+
+var RSU1 = FilterControl.isUnitTypeAllowed;
+FilterControl.isUnitTypeAllowed = function(unit, targetUnit){
+	var result = RSU1.call(this, unit, targetUnit);
+	if (targetUnit.custom.Untouchable){
+		return false;
 	}
-	return unit;
+	return result;
+};
+
+var RSU2 = FilterControl.isReverseUnitTypeAllowed;
+FilterControl.isReverseUnitTypeAllowed = function(unit, targetUnit){
+	var result = RSU2.call(this, unit, targetUnit);
+	if (targetUnit.custom.Untouchable){
+		return false;
+	}
+	return result;
+};
+
+var RSU3 = FilterControl.isBestUnitTypeAllowed;
+FilterControl.isBestUnitTypeAllowed = function(unit, targetUnit, filterFlag){
+	var result = RSU3.call(this, unit, targetUnit, filterFlag);
+	if (targetUnit.custom.Untouchable){
+		return false;
+	}
+	return result;
 };
