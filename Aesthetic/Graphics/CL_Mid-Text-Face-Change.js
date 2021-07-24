d1d4d30416d66ce712a52032b9fc75a721803122
Claris <seventh_sage@hotmail.com>
2021-07-24 16:42:23 -0400

    Face Changing now unlimited

--- Aesthetic/Graphics/CL_Mid-Text-Face-Change.js
+++ Aesthetic/Graphics/CL_Mid-Text-Face-Change.js
@@ -11,9 +11,7 @@
  * -RogueClaris
  *
  * =Plugin History=
- * July 24th, 2021: Updated for more dynamic control detection.
  * July 22nd, 2021: Released.
- *
  */
 
 var MidTextFaceChangeCL0 = TextParser._configureVariableObject;
@@ -25,12 +23,12 @@ TextParser._configureVariableObject = function(groupArray){
 ControlVariable.FaceChange = defineObject(BaseControlVariable,
 {
 	checkDrawInfo: function(index, objectArray, drawInfo) {
+		//fetch the object, this is RTP code, I really don't know what it does.
 		var obj = this.getObjectFromIndex(index, objectArray, this);
-		
+		//check if it's null; it won't do us any good if it is.
 		if (obj === null) {
 			return;
 		}
-		
 		drawInfo.faceChanger = obj;
 	},
 	
@@ -44,17 +42,40 @@ ControlVariable.FaceChange = defineObject(BaseControlVariable,
 
 var MidTextFaceChangeCL1 = BaseMessageView.drawMessageView;
 BaseMessageView.drawMessageView = function(isActive, pos){
+	//snag the analyzers.
 	var analyzer1 = this._messageAnalyzer;
 	var analyzer2 = this._messageAnalyzer.getCoreAnalyzer();
-	var textLine = analyzer2._textLineArray[analyzer2._rowCount];
+	//get the current line of text's object,
+	var textLine = analyzer2._textLineArray[analyzer2._rowCount].text;
+	//and its text.
 	var txt = textLine.text;
+	//and the current index.
 	var index = textLine.currentIndex;
-	var parser = analyzer2._textParser;
+	//grab base index, aka bindex.
 	var bindex = textLine.baseIndex;
-	if (analyzer1._parserInfo.changeFaceId && analyzer1._parserInfo.changeFaceIndex){
-		index = Math.max(0, index - (analyzer1._parserInfo.changeFaceId[0].toString().length));
-		if (bindex+index === analyzer1._parserInfo.changeFaceIndex){
-			this._faceId = analyzer1._parserInfo.changeFaceId[1];
+	//pull up the array.
+	var changeFace = analyzer1._parserInfo._changeFaceCL;
+	//pull up the backup text.
+	var backup = analyzer1._parserInfo._backupTextCL;
+	//initialize cindex [change index] and poppy [pop but it's actually shift]
+	var cindex, poppy;
+	//create a duplicate of changeFace every time. this way we don't run out of array before we hit the index.
+	var changeFace2 = [].concat(changeFace);
+	//once it's around,
+	if (changeFace2){
+		//shift out the first element copied over.
+		poppy = changeFace2.shift();
+		//if THAT'S found,
+		if (poppy){
+			//check the index of it in the backup text.
+			cindex = backup.indexOf(poppy)
+			//if the change index is not -1, then...
+			if (cindex !== -1 && cindex === index+bindex){
+				//shift out the first element of the ORIGINAL array to change what we're accessing, and...
+				changeFace.shift()
+				//set faceId to the number found within poppy.
+				this._faceId = poppy.match(/[0-9]+/)
+			}
 		}
 	}
 	MidTextFaceChangeCL1.call(this, isActive, pos);
@@ -62,13 +83,14 @@ BaseMessageView.drawMessageView = function(isActive, pos){
 
 var MidTextFaceChangeCL2 = TextParser.startReplace;
 TextParser.startReplace = function(text, parserInfo){
-	var checkIndex = text.search(ControlVariable.FaceChange.getKey())
-	if (checkIndex !== -1){
-		var findNum = text.match(ControlVariable.FaceChange.getKey())
-		parserInfo.changeFaceIndex = checkIndex;
-		if (findNum !== null){
-			parserInfo.changeFaceId = findNum;
-		}
+	//find all instances of \fc[x] in the text, where x is a number.
+	var findNum = text.match(/\\fc\[(\d+)\]/g)
+	//make sure it's not null and that it has at least 1 result.
+	if (findNum && findNum.length > 0){
+		//backup the original text
+		parserInfo._backupTextCL = text;
+		//save the match.
+		parserInfo._changeFaceCL = findNum;
 	}
 	return MidTextFaceChangeCL2.call(this, text, parserInfo);
 };
