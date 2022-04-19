38755221a29abc8d95a18996e94b8d2c3abfc3b6
RogueClaris <seventh_sage@hotmail.com>
2022-04-19 13:36:55 -0400

    New Script!

---
+++ Aesthetic/UI/Text/CL_Adjust-Message-Spacing.js
@@ -0,0 +1,7 @@
+var CLSpacingAdjust001 = CoreAnalyzer.getCharSpaceHeight;
+CoreAnalyzer.getCharSpaceHeight = function() {
+    return CLSpacingAdjust001.call(this) - 5; //Base is font height plus 10 pixels. This takes 5 pixels away again. You can change the 5 and the minus as you please.
+};
+
+var MessageRowCount = 4; //adjust this 4 to adjust the number of lines in a text box. Defaults is 3.
+CoreAnalyzer._maxRowCount = MessageRowCount; //Do not edit this line.
