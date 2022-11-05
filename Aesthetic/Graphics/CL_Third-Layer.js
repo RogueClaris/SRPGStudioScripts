5342cf3cc768906fb923611ad9bc31e11dbfbd21
Dawn <seventh_sage@hotmail.com>
2022-11-05 14:19:54 -0400

    Third layer bugfix update

--- Aesthetic/Graphics/CL_Third-Layer.js
+++ Aesthetic/Graphics/CL_Third-Layer.js
@@ -23,109 +23,105 @@
   Adapted Plugin History:
   2020/10/05 Released
 
-  2022/11/02 Update
-	No longer leaves old map layer artifacts on the next map.
- 	Overrides the following commands for Quality of Life visual concerns:
-	  AfterTransitionFlowEntry._completeMemberData;
-	  BeforeTransitionFlowEntry._completeMemberData;
-	
+  	2022/11/02 Update
+		No longer leaves old map layer artifacts on the next map.
+	  	Overrides the following commands for Quality of Life visual concerns:
+	  	AfterTransitionFlowEntry._completeMemberData;
+	  	BeforeTransitionFlowEntry._completeMemberData;
+   
+	2022/11/05 Update
+		No longer lags horrifically. Again. Fixed that AGAIN.
+		No longer leaves artifacts...again...I fixed that in a different way. Hopefully for good.
   
 --------------------------------------------------------------------------*/
 
-(function() {
-var alias3 = MapLayer.drawUnitLayer;
-MapLayer.drawUnitLayer = function() {
-	alias3.call(this);
-	var i, j;
-	var session = root.getCurrentSession();
-	var mx = session.getScrollPixelX();
-	var my = session.getScrollPixelY();
-	if (root.getMetaSession().global.ThirdLayerArray == null){
-		root.getMetaSession().global.ThirdLayerArray = []
-	}
-	if (this._mx !== mx || this._my !== my){
-		for (i = 0; i < CurrentMap.getWidth(); i++){
-			for (j = 0; j < CurrentMap.getHeight(); j++){
-				var Terrain = PosChecker.getTerrainFromPos(i, j)
-				if (Terrain.custom.StealthCL){
-					this._mx = mx
-					this._my = my
-					var img = Terrain.getMapchipImage()
-					var handle = session.getMapChipGraphicsHandle(i, j, true);
-					if (MapView.isVisiblePixel(i*root.getMapchipWidth(), (j*root.getMapchipHeight())+root.getMapchipHeight())){
-						if (my > 0){
-							root.getMetaSession().global.ThirdLayerArray.push([img, LayoutControl.getPixelX(i), (root.getMapchipWidth() * j) - my, handle.getSrcX() * root.getMapchipWidth(), handle.getSrcY() * root.getMapchipHeight(), root.getMapchipWidth(), root.getMapchipHeight()])
-						}
-						else{
-							root.getMetaSession().global.ThirdLayerArray.push([img, LayoutControl.getPixelX(i), (root.getMapchipWidth() * j), handle.getSrcX() * root.getMapchipWidth(), handle.getSrcY() * root.getMapchipHeight(), root.getMapchipWidth(), root.getMapchipHeight()])
-						}
+(function () {
+	var alias3 = MapLayer.drawUnitLayer;
+	MapLayer.drawUnitLayer = function () {
+		alias3.call(this);
+		var i, j;
+		var session = root.getCurrentSession();
+		var mx = session.getScrollPixelX();
+		var my = session.getScrollPixelY();
+		if (root.getMetaSession().global.ThirdLayerArray == null) {
+			root.getMetaSession().global.ThirdLayerArray = []
+			for (i = 0; i < CurrentMap.getWidth(); i++) {
+				for (j = 0; j < CurrentMap.getHeight(); j++) {
+					var Terrain = PosChecker.getTerrainFromPos(i, j)
+					if (Terrain.custom.StealthCL) {
+						var img = Terrain.getMapchipImage()
+						var handle = session.getMapChipGraphicsHandle(i, j, true);
+						root.getMetaSession().global.ThirdLayerArray.push([img, i, j, handle.getSrcX() * GraphicsFormat.MAPCHIP_WIDTH, handle.getSrcY() * GraphicsFormat.MAPCHIP_HEIGHT, GraphicsFormat.MAPCHIP_WIDTH, GraphicsFormat.MAPCHIP_HEIGHT])
 					}
 				}
 			}
 		}
-	}
-	var t;
-	var arr = root.getMetaSession().global.ThirdLayerArray
-	for (t = 0; t < arr.length; ++t){
-		arr[t][0].drawParts(arr[t][1], arr[t][2], arr[t][3], arr[t][4], arr[t][5], arr[t][6])
-	}
-};
+		var t;
+		var arr = root.getMetaSession().global.ThirdLayerArray
+		for (t = 0; t < arr.length; ++t) {
+			arr[t][0].drawParts((arr[t][1] * GraphicsFormat.MAPCHIP_WIDTH) - mx, (arr[t][2] * GraphicsFormat.MAPCHIP_HEIGHT) - my, arr[t][3], arr[t][4], arr[t][5], arr[t][6])
+		}
+	};
 
-var DrawScrollCL = UnitRenderer.drawScrollUnit;
-UnitRenderer.drawScrollUnit = function(unit, x, y, unitRenderParam) {
-	DrawScrollCL.call(this, unit, x, y, unitRenderParam);
-	var i, j;
-	var t;
-	var arr = root.getMetaSession().global.ThirdLayerArray
-	for (t = 0; t < arr.length; ++t){
-		arr[t][0].drawParts(arr[t][1], arr[t][2], arr[t][3], arr[t][4], arr[t][5], arr[t][6])
-	}
-};
+	var DrawScrollCL = UnitRenderer.drawScrollUnit;
+	UnitRenderer.drawScrollUnit = function (unit, x, y, unitRenderParam) {
+		DrawScrollCL.call(this, unit, x, y, unitRenderParam);
+		var session = root.getCurrentSession();
+		var mx = session.getScrollPixelX();
+		var my = session.getScrollPixelY();
+		var t;
+		var arr = root.getMetaSession().global.ThirdLayerArray
+		for (t = 0; t < arr.length; ++t) {
+			for (t = 0; t < arr.length; ++t) {
+				arr[t][0].drawParts((arr[t][1] * GraphicsFormat.MAPCHIP_WIDTH) - mx, (arr[t][2] * GraphicsFormat.MAPCHIP_HEIGHT) - my, arr[t][3], arr[t][4], arr[t][5], arr[t][6])
+			}
+		}
+	};
 
-var EraseArrCL = ScriptCall_Load;
-ScriptCall_Load = function(){
-	EraseArrCL.call(this);
-	if (root.getMetaSession().global.ThirdLayerArray != null){
-		delete root.getMetaSession().global.ThirdLayerArray
+	var EraseArrCL = ScriptCall_Load;
+	ScriptCall_Load = function () {
+		EraseArrCL.call(this);
+		if (root.getMetaSession().global.ThirdLayerArray != null) {
+			delete root.getMetaSession().global.ThirdLayerArray
+		}
 	}
-}
 
-var EraseMapLayerCL1 = SystemTransition.initialize;
-SystemTransition.initialize = function(){
-	EraseMapLayerCL1.call(this);
-	if (root.getMetaSession().global.ThirdLayerArray != null && root.getCurrentScene() === SceneType.BATTLESETUP){
-		delete root.getMetaSession().global.ThirdLayerArray
+	var EraseMapLayerCL1 = SystemTransition.initialize;
+	SystemTransition.initialize = function () {
+		EraseMapLayerCL1.call(this);
+		if (root.getMetaSession().global.ThirdLayerArray != null && root.getCurrentScene() === SceneType.BATTLESETUP) {
+			delete root.getMetaSession().global.ThirdLayerArray
+		}
 	}
-}
 
-AfterTransitionFlowEntry._completeMemberData = function(battleResultScene) {
-	if (SceneManager.isScreenFilled()) {
-		return EnterResult.NOTENTER;
-	}
-	
-	this._transition.setFadeSpeed(5);
-	this._transition.setEffectRangeType(EffectRangeType.ALL);
-	this._transition.setVolume(255, 0, 160, true);
-	
-	return EnterResult.OK;
-}
+	AfterTransitionFlowEntry._completeMemberData = function (battleResultScene) {
+		if (SceneManager.isScreenFilled()) {
+			return EnterResult.NOTENTER;
+		}
 
-BeforeTransitionFlowEntry._completeMemberData = function(battleResultScene) {
-	var effect;
-	
-	if (SceneManager.isScreenFilled()) {
-		// If it's EffectRangeType.ALL, the characters on the "logo" are all covered,
-		// so change it to EffectRangeType.ALL.
-		effect = root.getScreenEffect();
-		effect.setRange(EffectRangeType.ALL);
-		return EnterResult.NOTENTER;
+		this._transition.setFadeSpeed(5);
+		this._transition.setEffectRangeType(EffectRangeType.ALL);
+		this._transition.setVolume(255, 0, 160, true);
+
+		return EnterResult.OK;
 	}
-	
-	this._transition.setFadeSpeed(5);
-	this._transition.setEffectRangeType(EffectRangeType.ALL);
-	this._transition.setVolume(160, 0, 0, true);
-	
-	return EnterResult.OK;
-}
+
+	BeforeTransitionFlowEntry._completeMemberData = function (battleResultScene) {
+		var effect;
+
+		if (SceneManager.isScreenFilled()) {
+			// If it's EffectRangeType.ALL, the characters on the "logo" are all covered,
+			// so change it to EffectRangeType.ALL.
+			effect = root.getScreenEffect();
+			effect.setRange(EffectRangeType.ALL);
+			return EnterResult.NOTENTER;
+		}
+
+		this._transition.setFadeSpeed(5);
+		this._transition.setEffectRangeType(EffectRangeType.ALL);
+		this._transition.setVolume(160, 0, 0, true);
+
+		return EnterResult.OK;
+	}
 
 })();
